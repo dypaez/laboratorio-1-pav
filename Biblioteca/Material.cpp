9892a8e7e2466fb083ef856bb3ff608417fedd27
Matías Acuña <matuzsjj@gmail.com>
2026-03-21 19:41:35 -0300

    Material

---
+++ Biblioteca/Material.cpp
@@ -0,0 +1,33 @@
+#include "Material.h"
+using namespace std;
+Material::Material(string codigo, string titulo, int anioPublicacion){
+    this->codigo = codigo;
+    this->titulo = titulo;
+    this->anioPublicacion = anioPublicacion;
+
+}
+string Material::getCodigo(){
+    return this->codigo;
+}
+string Material::getTitulo(){
+    return this->titulo;
+}
+int Material::getAnioPublicacion(){
+    return this->anioPublicacion;
+}
+
+void Material::setCodigo(string codigo) {
+    this->codigo = codigo;
+}
+
+void Material::setTitulo(string titulo) {
+    this->titulo = titulo;
+}
+
+void Material::setAnioPublicacion(int anioPublicacion) {
+    this->anioPublicacion = anioPublicacion;
+}
+
+Material::~Material(){
+
+}
---
+++ Biblioteca/Material.h
@@ -0,0 +1,25 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+#include <string>
+#include <iostream>
+class Material{
+	private:
+		std::string codigo;
+		std::string titulo;
+		int anioPublicacion;
+		
+	public:
+	
+		Material(std::string codigo, std::string titulo, int anioPublicacion);
+	
+		std::string getCodigo();
+		std::string getTitulo();
+		int getAnioPublicacion();
+		void setCodigo(std::string);
+        void setTitulo(std::string);
+        void setAnioPublicacion(int);
+	
+		~Material();
+};
+
+#endif
