3466fc84b8262992fa4b1b6325b8cd1749c5b37b
Dami <80689629+dypaez@users.noreply.github.com>
2026-03-26 20:42:33 -0300

    Update main.cpp

--- Biblioteca/main.cpp
+++ Biblioteca/main.cpp
@@ -11,154 +11,39 @@ using namespace std;
 /*
 ARREGLOS
 */
-    Lector* lectores[MAX_LECTORES];
-    Material* materiales[MAX_MATERIALES];
-    
-/*
-    ARREGLOS
-*/
+Lector* lectores[MAX_LECTORES];
+Material* materiales[MAX_MATERIALES];
 
-int main(){
-    //INICIALIZACION DE ARREGLOS
-    for(int i=0; i<MAX_MATERIALES; i++){
-        materiales[i] = nullptr;
-    }
+/*
+ARREGLOS
+*/
+bool existeLector(string ci){
     for(int i=0; i<MAX_LECTORES; i++){
-        lectores[i] = nullptr;
-    }
-    int opcion=-1;
-    while(opcion!=0){
-        setlocale(LC_ALL, "");
-        mostrarMenu();
-        cout << "Opción: ";
-        cin >> opcion;
-        switch(opcion){
-            case 0:
-            break;
-            case 1:{
-                string ci = "", nombre = "", fecha = "";
-                string diaStr, mesStr, anioStr;
-                DtFecha* fechaRegistro = nullptr;
-                cout << "\nIngrese la cédula de el/la lector/a";
-                cin >> ci;
-                cout << "\nIngrese el nombre de el/la lector/a";
-                cin >> nombre;
-                cout << "\nIngrese la fecha de registro (Formato DD/MM/YYYY):";
-                cin >> fecha;
-                stringstream ss(fecha);
-                getline(ss, diaStr, '/');
-                getline(ss, mesStr, '/');
-                getline(ss, anioStr, '/');
-                int dia = stoi(diaStr);
-                int mes = stoi(mesStr);
-                int anio = stoi(anioStr);
-                fechaRegistro = new DtFecha(dia, mes, anio);
-                registrarLector(ci, nombre, fechaRegistro);
-            break;}
-            case 2:{
-                string ci = "", codigo = "", fecha = "";
-                string diaStr, mesStr, anioStr;
-                int duracionPrestamo;
-                DtFecha* fechaPrestamo;
-                cout << "\nIngrese la cédula del lector prestatario: \n > ";
-                cin >> ci;
-                cout << "\nIngrese el código del material que se prestará: \n > ";
-                cin >> codigo;
-                cout << "\nIngrese la fecha de registro (Formato DD/MM/YYYY): \n > ";
-                cin >> fecha;
-                stringstream ss(fecha);
-                getline(ss, diaStr, '/');
-                getline(ss, mesStr, '/');
-                getline(ss, anioStr, '/');
-                int dia = stoi(diaStr);
-                int mes = stoi(mesStr);
-                int anio = stoi(anioStr);
-                fechaPrestamo = new DtFecha(dia, mes, anio);
-                cout << "\nIngrese la duración en días del préstamo: \n > ";
-                cin >> duracionPrestamo;
-                agregarPrestamo(ci, codigo, fechaPrestamo, duracionPrestamo);
-            break;}
-            case 3:
-            break;
-            case 4:
-            break;
-            case 5:
-            break;
-            case 6:{
-                DtMaterial* mat = nullptr;
-                int opc_material = 0;
-                while((opc_material < 1) || (opc_material>2)){
-                    cout << "¿Qué tipo de Material desea ingresar?" << endl <<
-                            "1) Libro" << endl <<
-                            "2) Revista" << endl <<
-                            "\t> ";
-                    cin >> opc_material;
-                    if(opc_material < 1 || opc_material > 2){
-                        cout << "Opción inválida. Vuelta a intentarlo." << endl;
-                    }
-                }
-                string codigo, titulo;
-                int anio;
-                cout << "Ingrese el código del material: ";
-                cin >> codigo;
-                cout << "\nIngrese el título del material:";
-                cin >> titulo;
-                cout << "\nIngrese el año de publicación del material:";
-                cin >> anio;
-                switch(opc_material){
-                        case 1:
-                        {
-                            string autor;
-                            int cantPag;
-                            cout << "\n¿Quién es el autor del libro?: ";
-                            cin >> autor;
-                            cout << "\n¿Cuántas páginas tiene el libro?";
-                            cin >> cantPag;
-                            mat = new DtLibro(codigo, titulo, autor, anio, cantPag);
-                            break;
-                        }
-                        case 2:
-                        {
-                            int numeroEdicion;
-                            char SiNo='0';
-                            bool esMensual;
-                            cout << "\nIngrese el número de edición de la revista: ";
-                            cin >> numeroEdicion;
-                            do{
-                                cout << "\n¿Es mensual? (S/N)";
-                                cin >> SiNo;
-                                SiNo = toupper(SiNo);
-                                if(SiNo != 'N' && SiNo != 'S'){
-                                    cout << "\nOpción inválida. Intente de nuevo." << endl;
-                                }
-                            }while(SiNo != 'N' && SiNo != 'S');
-                            esMensual = (SiNo == 'S');
-                            mat = new DtRevista(codigo, titulo, numeroEdicion, anio, esMensual);
-                        }
-                        break;
-                }
-                agregarMaterial(mat);
-                delete mat;
-                break;}
-            default:
-            break;
+        if(lectores[i]!=nullptr && lectores[i]->getCi()==ci){
+            return true;
         }
     }
-    return 0;
+    return false;
 }
-
+bool existeMaterial(string codigo){
+    for(int i=0; i<MAX_MATERIALES; i++){
+        if(materiales[i]!=nullptr && materiales[i]->getCodigo() == codigo){
+            return true;
+        }
+    }
+    return false;
+}
 void mostrarMenu(){
     setlocale(LC_ALL, "");
     cout << "Bienvenido!\nElija la opción:" << endl <<
-            "1) Registrar lector" << endl <<
-            "2) Agregar préstamo" << endl <<
-            "3) Obtener materiales prestados" << endl <<
+    "1) Registrar lector" << endl <<
+    "2) Agregar préstamo" << endl <<
+    "3) Obtener materiales prestados" << endl <<
             "4) Consultar multa de material" << endl <<
             "5) Ver préstamos antes de fecha" << endl <<
             "6) Agregar material" << endl <<
             "0) Salir" << endl;
 }
-
 void registrarLector(string ci, string nombre, DtFecha* fechaRegistro){
     if(existeLector(ci)){ //Si ya existe Lector registrado con la CI pasada por parametro
         throw invalid_argument("Ya existe un usuario registrado con esa CI.");
@@ -254,7 +139,7 @@ DtMaterial** verPrestamosAntesDeFecha(string ci, DtFecha* fecha, int& cantPresta
     throw invalid_argument("El lector ingresado no existe."); //Lanzar error.
 }
 void agregarMaterial(DtMaterial * dtMaterial){
-    if(existeMaterial(dtMaterial->getCodigo())){ //Esto no creo que amerite comentarios.
+    if(dtMaterial!=nullptr && existeMaterial(dtMaterial->getCodigo())){ //Esto no creo que amerite comentarios.
         throw invalid_argument("Ya existe un material con ese código.");
     }
     for(int i=0; i<MAX_MATERIALES; i++){ //Recorriendo la colección de materiales...
@@ -274,20 +159,132 @@ void agregarMaterial(DtMaterial * dtMaterial){
     }//En caso de que no haya espacios nulos...
     throw invalid_argument("La colección de materiales está llena."); //Lanzar error.
 }
-bool existeLector(string ci){
-    for(int i=0; i<MAX_LECTORES; i++){
-        if(lectores[i]!=nullptr && lectores[i]->getCi()==ci){
-            return true;
-        }
-    }
-    return false;
+int main(){
+//INICIALIZACION DE ARREGLOS
+for(int i=0; i<MAX_MATERIALES; i++){
+    materiales[i] = nullptr;
 }
-bool existeMaterial(string codigo){
-    for(int i=0; i<MAX_MATERIALES; i++){
-        if(materiales[i]!=nullptr && materiales[i]->getCodigo() == codigo){
-            return true;
-        }
+for(int i=0; i<MAX_LECTORES; i++){
+    lectores[i] = nullptr;
+}
+int opcion=-1;
+while(opcion!=0){
+    setlocale(LC_ALL, "");
+    mostrarMenu();
+    cout << "Opción: ";
+    cin >> opcion;
+    switch(opcion){
+        case 0:
+        break;
+        case 1:{
+            string ci = "", nombre = "", fecha = "";
+            string diaStr, mesStr, anioStr;
+            DtFecha* fechaRegistro = nullptr;
+            cout << "\nIngrese la cédula de el/la lector/a";
+            cin >> ci;
+            cout << "\nIngrese el nombre de el/la lector/a";
+            cin >> nombre;
+            cout << "\nIngrese la fecha de registro (Formato DD/MM/YYYY):";
+            cin >> fecha;
+            stringstream ss(fecha);
+            getline(ss, diaStr, '/');
+            getline(ss, mesStr, '/');
+            getline(ss, anioStr, '/');
+            int dia = stoi(diaStr);
+            int mes = stoi(mesStr);
+            int anio = stoi(anioStr);
+            fechaRegistro = new DtFecha(dia, mes, anio);
+            registrarLector(ci, nombre, fechaRegistro);
+        break;}
+        case 2:{
+            string ci = "", codigo = "", fecha = "";
+            string diaStr, mesStr, anioStr;
+            int duracionPrestamo;
+            DtFecha* fechaPrestamo;
+            cout << "\nIngrese la cédula del lector prestatario: \n > ";
+            cin >> ci;
+            cout << "\nIngrese el código del material que se prestará: \n > ";
+            cin >> codigo;
+            cout << "\nIngrese la fecha de registro (Formato DD/MM/YYYY): \n > ";
+            cin >> fecha;
+            stringstream ss(fecha);
+            getline(ss, diaStr, '/');
+            getline(ss, mesStr, '/');
+            getline(ss, anioStr, '/');
+            int dia = stoi(diaStr);
+            int mes = stoi(mesStr);
+            int anio = stoi(anioStr);
+            fechaPrestamo = new DtFecha(dia, mes, anio);
+            cout << "\nIngrese la duración en días del préstamo: \n > ";
+            cin >> duracionPrestamo;
+            agregarPrestamo(ci, codigo, fechaPrestamo, duracionPrestamo);
+        break;}
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:{
+            DtMaterial* mat = nullptr;
+            int opc_material = 0;
+            while((opc_material < 1) || (opc_material>2)){
+                cout << "¿Qué tipo de Material desea ingresar?" << endl <<
+                        "1) Libro" << endl <<
+                        "2) Revista" << endl <<
+                        "\t> ";
+                cin >> opc_material;
+                if(opc_material < 1 || opc_material > 2){
+                    cout << "Opción inválida. Vuelta a intentarlo." << endl;
+                }
+            }
+            string codigo, titulo;
+            int anio;
+            cout << "Ingrese el código del material: ";
+            cin >> codigo;
+            cout << "\nIngrese el título del material:";
+            cin >> titulo;
+            cout << "\nIngrese el año de publicación del material:";
+            cin >> anio;
+            switch(opc_material){
+                    case 1:
+                    {
+                        string autor;
+                        int cantPag;
+                        cout << "\n¿Quién es el autor del libro?: ";
+                        cin >> autor;
+                        cout << "\n¿Cuántas páginas tiene el libro?";
+                        cin >> cantPag;
+                        mat = new DtLibro(codigo, titulo, autor, anio, cantPag);
+                        break;
+                    }
+                    case 2:
+                    {
+                        int numeroEdicion;
+                        char SiNo='0';
+                        bool esMensual;
+                        cout << "\nIngrese el número de edición de la revista: ";
+                        cin >> numeroEdicion;
+                        do{
+                            cout << "\n¿Es mensual? (S/N)";
+                            cin >> SiNo;
+                            SiNo = toupper(SiNo);
+                            if(SiNo != 'N' && SiNo != 'S'){
+                                cout << "\nOpción inválida. Intente de nuevo." << endl;
+                            }
+                        }while(SiNo != 'N' && SiNo != 'S');
+                        esMensual = (SiNo == 'S');
+                        mat = new DtRevista(codigo, titulo, numeroEdicion, anio, esMensual);
+                    }
+                    break;
+            }
+            agregarMaterial(mat);
+            delete mat;
+            break;}
+        default:
+        break;
     }
-    return false;
+}
+return 0;
 }
 
